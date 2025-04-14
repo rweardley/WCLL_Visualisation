0ca90b6a9c2f71f1251b8fab4f15d7ae5f1dbcba
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-04-14 18:40:35 +0100

    Add cosma test scripts (works well)

---
+++ cosma/test.py
@@ -0,0 +1,34 @@
+# pvbatch script for resampling NEK5000 data
+from paraview.simple import *
+import time
+
+t_init=time.time()
+
+time.sleep(10)
+
+print(f"Checkpoint: 1, time={time.time()-t_init:.2f} s")
+
+time.sleep(10)
+
+print(f"Checkpoint: 2, time={time.time()-t_init:.2f} s")
+
+time.sleep(10)
+
+print(f"Checkpoint: 3, time={time.time()-t_init:.2f} s")
+
+time.sleep(10)
+
+print(f"Checkpoint: 4, time={time.time()-t_init:.2f} s")
+
+time.sleep(10)
+
+print(f"Checkpoint: 5, time={time.time()-t_init:.2f} s")
+
+time.sleep(10)
+
+print(f"Checkpoint: 6, time={time.time()-t_init:.2f} s")
+
+time.sleep(10)
+
+print(f"Checkpoint: 7, time={time.time()-t_init:.2f} s")
+
