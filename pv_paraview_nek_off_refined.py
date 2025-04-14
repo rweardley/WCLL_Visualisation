131d8b56eed9140bd97e6970dc7dda1a67f3ac87
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2025-04-14 08:55:42 +0100

    Add scripts for sampling data to structured grid for MHD off case

---
+++ pv_paraview_nek_off_refined.py
@@ -0,0 +1,43 @@
+# pvbatch script for resampling NEK5000 data
+from paraview.simple import *
+import time
+
+t_init=time.time()
+
+print(f"Checkpoint: 1, time={time.time()-t_init:.2f} s")
+
+# File path and output configuration
+
+input_file = "/home/ir-eard1/rds/rds-ukaea-ap001/ir-eard1/NekRS/user_problems/WCLL-ORNL/cht63M/globus/run3_mhd_off_N7/pink.nek5000"
+output_file = input_file.replace("pink.nek5000", "pink_resampled_2850_700_4760.pvti")
+
+print(f"Checkpoint: 2, time={time.time()-t_init:.2f} s")
+
+# Load NEK5000 data
+nek5000_data = Nek5000Reader(FileName=input_file)
+
+print(f"Checkpoint: 3, time={time.time()-t_init:.2f} s")
+
+# Update pipeline to get metadata (e.g., bounds)
+nek5000_data.UpdatePipeline()
+
+print(f"Checkpoint: 4, time={time.time()-t_init:.2f} s")
+
+# Apply Resample To Image filter
+resample = ResampleToImage(Input=nek5000_data)
+resample.UseInputBounds = 1
+resample.SamplingDimensions = [2850, 700, 4760]
+
+print(f"Checkpoint: 5, time={time.time()-t_init:.2f} s")
+
+# Update pipeline for resampling
+resample.UpdatePipeline()
+
+print(f"Checkpoint: 6, time={time.time()-t_init:.2f} s")
+
+
+# Save output as .vti (VTK ImageData format)
+SaveData(output_file, proxy=resample, FileType='VTK XML Image Data', Writetimestepsasfileseries=1, Filenamesuffix='_%.3d')
+
+print(f"Checkpoint: 7, time={time.time()-t_init:.2f} s")
+
