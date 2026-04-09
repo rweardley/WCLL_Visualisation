e751adabb65e57f60bc63e74e3e152665af72f1a
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-04-09 19:22:01 +0100

    Undo last changes

--- olcf/dat_mhd_off_m4_r8_N5.py
+++ olcf/dat_mhd_off_m4_r8_N5.py
@@ -11,8 +11,6 @@ ts_idx = int(sys.argv[1])
 print(f"[{ts_idx}] >>> Timestep index: {ts_idx}")
 filter_domains = bool(sys.argv[2])
 print(f"[{ts_idx}] >>> Filter domains: {filter_domains}")
-polynomial_order = bool(sys.argv[3])
-print(f"[{ts_idx}] >>> Polynomial order: {polynomial_order}")
 
 print(f"[{ts_idx}] >>> Checkpoint: 1, time={time.time()-t_init:.2f} s")
 
@@ -21,30 +19,19 @@ print(f"[{ts_idx}] >>> Checkpoint: 1, time={time.time()-t_init:.2f} s")
 input_file = "/lustre/orion/fus166/proj-shared/ylan/vis_rupert_gb26/dat_mhd_off_m4_r8_N5/pink.nek5000"
 output_file = "/lustre/orion/fus166/proj-shared/rweb/dat_mhd_off_m4_r8_N5_x400"
 full_sampling_dimensions = [11097, 400, 1042]
-elementIDs = [
-    -1 * polynomial_order**3,
-    38733372 * polynomial_order**3,
-    140869280 * polynomial_order**3,
-    151935592 * polynomial_order**3,
-    245637464 * polynomial_order**3,
-    257676220 * polynomial_order**3,
-]
+spectralIDs = [-1, 38733372, 140869280, 151935592, 245637464, 257676220]
 domainNames = ["water_TBM", "PbLi", "water_shield", "solid_TBM", "solid_shield"]
 
 print(f"[{ts_idx}] >>> Checkpoint: 2, time={time.time()-t_init:.2f} s")
 
 # Load NekRS data
 nek5000_data = Nek5000Reader(FileName=input_file)
+nek5000_data.AddSpectralElementIdsasCellData = 1
 
 print(f"[{ts_idx}] >>> Checkpoint: 3, time={time.time()-t_init:.2f} s")
 
 # Update pipeline to execute data loading for current timestep
 nek5000_data.UpdatePipeline(time=nek5000_data.TimestepValues[ts_idx])
-
-pointAndCellIds = PointAndCellIds(Input=nek5000_data)
-pointAndCellIds.GeneratePointIds = 0
-pointAndCellIds.GenerateCellIds = 1
-pointAndCellIds.UpdatePipeline()
 
 gc.collect()
 
@@ -56,38 +43,29 @@ print(f"[{ts_idx}] >>> Commencing loop through domains")
 
 for domain in range(len(domainNames)):
     if filter_domains:
-        threshold = Threshold(
-            registrationName=f"Threshold_{ts_idx}_{domain}", Input=nek5000_data
-        )
-        print(
-            f"[{ts_idx}] >>> Domain {domain}: {domainNames[domain]}, time={time.time()-t_init:.2f} s"
-        )
-        print(
-            f"[{ts_idx}] >>> Filtering by Element ID, time={time.time()-t_init:.2f} s"
-        )
+        threshold = Threshold(registrationName=f"Threshold_{ts_idx}_{domain}", Input=nek5000_data)
+        print(f"[{ts_idx}] >>> Domain {domain}: {domainNames[domain]}, time={time.time()-t_init:.2f} s")
+        print(f"[{ts_idx}] >>> Filtering by Spectral Element ID, time={time.time()-t_init:.2f} s")
         threshold.Set(
-            Scalars=["CELLS", "CellIds"],
-            LowerThreshold=elementIDs[domain] + polynomial_order**3,
-            UpperThreshold=elementIDs[domain + 1],
+            Scalars=["CELLS", "spectral element id"],
+            LowerThreshold=spectralIDs[domain] + 1,
+            UpperThreshold=spectralIDs[domain + 1],
         )
         threshold.UpdatePipeline()
 
         # Get domain SamplingBounds and SamplingDimensions
         domain_bounds = threshold.GetDataInformation().GetBounds()
-        x_ratio = abs(
-            (domain_bounds[1] - domain_bounds[0])
-            / (full_domain_bounds[1] - full_domain_bounds[0])
-        )
+        x_ratio = abs((domain_bounds[1] - domain_bounds[0]) / (
+            full_domain_bounds[1] - full_domain_bounds[0]
+        ))
         print(f"[{ts_idx}] >>> x_ratio={x_ratio}")
-        y_ratio = abs(
-            (domain_bounds[3] - domain_bounds[2])
-            / (full_domain_bounds[3] - full_domain_bounds[2])
-        )
+        y_ratio = abs((domain_bounds[3] - domain_bounds[2]) / (
+            full_domain_bounds[3] - full_domain_bounds[2]
+        ))
         print(f"[{ts_idx}] >>> y_ratio={y_ratio}")
-        z_ratio = abs(
-            (domain_bounds[5] - domain_bounds[4])
-            / (full_domain_bounds[5] - full_domain_bounds[4])
-        )
+        z_ratio = abs((domain_bounds[5] - domain_bounds[4]) / (
+            full_domain_bounds[5] - full_domain_bounds[4]
+        ))
         print(f"[{ts_idx}] >>> z_ratio={z_ratio}")
         domain_sampling_dimensions = [
             int(full_sampling_dimensions[0] * x_ratio),
@@ -97,15 +75,11 @@ for domain in range(len(domainNames)):
         print(f"[{ts_idx}] >>> domain_sampling_dimensions={domain_sampling_dimensions}")
         print(f"[{ts_idx}] >>> domain_bounds={domain_bounds}")
 
-        print(
-            f"[{ts_idx}] >>> Checkpoint: 4 ({domain}), time={time.time()-t_init:.2f} s"
-        )
+        print(f"[{ts_idx}] >>> Checkpoint: 4 ({domain}), time={time.time()-t_init:.2f} s")
         print(f"[{ts_idx}] >>> Resample to Image")
 
         # Apply Resample To Image filter
-        resample = ResampleToImage(
-            registrationName=f"Resample_{ts_idx}_{domain}", Input=threshold
-        )
+        resample = ResampleToImage(registrationName=f"Resample_{ts_idx}_{domain}", Input=threshold)
         resample.Set(
             UseInputBounds=0,
             SamplingBounds=domain_bounds,
@@ -132,7 +106,7 @@ for domain in range(len(domainNames)):
     output_filename = f"{output_file}_"
     if filter_domains:
         output_filename += f"{domainNames[domain]}_"
-    output_filename += f"{ts_idx:03d}.pvti"
+    output_filename += f'{ts_idx:03d}.pvti'
     SaveData(output_filename, proxy=resample)
 
     # Clean up
