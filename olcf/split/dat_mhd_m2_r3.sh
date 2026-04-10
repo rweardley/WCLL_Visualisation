47265a43a60c1ac2802c4e12bab69a498647555b
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-04-10 10:53:05 +0100

    Change to the slurm scripts I'm used to, add options so they can run the same python script for different meshes

--- olcf/split/dat_mhd_m2_r3.sh
+++
@@ -1,9 +0,0 @@
-module load cray-python/3.11.7
-
-set -x
-
-sbatch -A fus166 -q normal -t 00:30:00 \
-  -N1 --ntasks-per-node=56 \
-  -J split_flds \
-  -o log_%x-%j.out  \
-  --wrap='module load cray-python/3.11.7; srun --cpu-bind=cores --threads-per-core=1 --label python3 split_m2_v2.py'
