e728160d051b707be00213c00b1e3df149d45a17
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-08-24 18:02:04 +0100

    Add remaining link scripts

--- olcf/symlink/link_buo1_mhd0_mc0.sh
+++ olcf/symlink/link_buo1_mhd0_mc0.sh
@@ -1,8 +1,8 @@
-SOURCE_DIR="/lustre/orion/nfu106/proj-shared/gottems/GB26_finalist/03_m3_no_buo/03_m3_no_buo/31run/"
-OUTPUT_DIR="/lustre/orion/fus166/proj-shared/rweb/gb_final/buo0_mhd0_mc1/"
+SOURCE_DIR="/lustre/orion/nfu106/proj-shared/aldeia/09_GB26_finalist/02_m3/13run/"
+OUTPUT_DIR="/lustre/orion/fus166/proj-shared/rweb/gb_final/buo1_mhd0_mc0/"
 
-CASENAME="pink5m3_no_buo"
-CHECKPOINT=00058
+CASENAME="pink5m3"
+CHECKPOINT=00052
 
 echo "SOURCE_DIR=$SOURCE_DIR" > $OUTPUT_DIR/log.link
 echo "CHECKPOINT=$CHECKPOINT" >> $OUTPUT_DIR/log.link
---
+++ olcf/symlink/link_buo1_mhd1_mc1.sh
@@ -0,0 +1,20 @@
+SOURCE_DIR="/lustre/orion/nfu106/proj-shared/gottems/GB26_finalist/04_m3_mhd/43run/"
+OUTPUT_DIR="/lustre/orion/fus166/proj-shared/rweb/gb_final/buo1_mhd1_mc1/"
+
+CASENAME="pink5m3_mhd"
+CHECKPOINT=00041
+
+echo "SOURCE_DIR=$SOURCE_DIR" > $OUTPUT_DIR/log.link
+echo "CHECKPOINT=$CHECKPOINT" >> $OUTPUT_DIR/log.link
+
+ln -sf $SOURCE_DIR/${CASENAME}0.f${CHECKPOINT} $OUTPUT_DIR/${CASENAME}0.f00000
+cp $SOURCE_DIR/${CASENAME}.nek5000 $OUTPUT_DIR
+sed -i 's/^numtimesteps:.*/numtimesteps: 1/' $OUTPUT_DIR/${CASENAME}.nek5000
+
+ln -sf $SOURCE_DIR/mhd${CASENAME}0.f${CHECKPOINT} $OUTPUT_DIR/mhd${CASENAME}0.f00000
+cp $SOURCE_DIR/mhd${CASENAME}.nek5000 $OUTPUT_DIR
+sed -i 's/^numtimesteps:.*/numtimesteps: 1/' $OUTPUT_DIR/mhd${CASENAME}.nek5000
+
+ln -sf $SOURCE_DIR/curr${CASENAME}0.f${CHECKPOINT} $OUTPUT_DIR/curr${CASENAME}0.f00000
+cp $SOURCE_DIR/curr${CASENAME}.nek5000 $OUTPUT_DIR
+sed -i 's/^numtimesteps:.*/numtimesteps: 1/' $OUTPUT_DIR/curr${CASENAME}.nek5000
