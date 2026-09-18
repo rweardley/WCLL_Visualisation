84214954d380a82a56006c17ab4ca0add2ded567
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-09-18 10:25:47 +0100

    Add prefix

--- olcf/animations/buo0_mhd1_mc1/1_symlink/links.sh
+++ olcf/animations/buo0_mhd1_mc1/1_symlink/links.sh
@@ -5,4 +5,5 @@
   --input "/lustre/orion/nfu106/proj-shared/dalinger/inputs/lm-mhd/2.1-nekrs/05_m3_mhd_no_buo" \
   --output "/lustre/orion/fus166/proj-shared/rweb/gb_final/animation/buo0_mhd1_mc1" \
   --batch-size 20 \
+  --prefix pink5m3_mhd_no_buo \
   --force
