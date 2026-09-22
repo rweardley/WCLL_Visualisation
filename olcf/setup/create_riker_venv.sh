93135e92308e6a3e8fe836fd782247bd71fe4823
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-09-22 11:14:37 +0100

    Update venv script, wasn't working as intended before

--- olcf/setup/create_riker_venv.sh
+++ olcf/setup/create_riker_venv.sh
@@ -2,10 +2,30 @@
 
 # to be run on riker
 
+ENV_DIR="$HOME/riker_venv"
+
+# List packages here
+PACKAGES=(
+    numpy
+    mpi4py
+)
+
 module load Core/26.01
 module load python/3.14.5
-python -m venv $HOME/riker_venv
-source riker_venv/bin/activate
-pip install --upgrade pip
-pip install numpy
-pip install mpi4py
+
+# Setup environment
+if [ ! -d "$ENV_DIR" ]; then
+  echo "Creating virtual environment in '$ENV_DIR'..."
+  python3 -m venv "$ENV_DIR"
+
+  echo "Installing packages..."
+  "$ENV_DIR/bin/pip3" install "${PACKAGES[@]}"
+else
+  echo "Using existing virtual environment '$ENV_DIR'."
+fi
+
+# Activate environment
+# (only works when you run: source setup_env.sh)
+source "$ENV_DIR/bin/activate"
+
+echo "Environment active: $(python -c 'import sys; print(sys.executable)')"
