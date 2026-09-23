f1cc15677d8770727547c94ef238fcc5ea4fc5de
Rupert Eardley-Brunt <rupert.eardley-brunt@ukaea.uk>
2026-09-23 13:53:30 +0100

    Add download script, add *.png to gitignore

---
+++ olcf/animations/download_frames.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp -r riker.olcf.ornl.gov:/lustre/orion/fus166/proj-shared/rweb/gb_final/animation/buo0_mhd0_mc1/vol_fluids_velocity_all_jet_split/ buo0_mhd0_mc1/
+# scp -r riker.olcf.ornl.gov:/lustre/orion/fus166/proj-shared/rweb/gb_final/animation/buo0_mhd1_mc1/vol_fluids_velocity_all_jet_split/ buo0_mhd1_mc1/
